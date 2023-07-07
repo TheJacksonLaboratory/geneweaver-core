1e154a61b80a43db67f7a683095098adb4907a6a
Alexander Berger <alexander.berger@jax.org>
2023-07-07 17:10:37 -0400

    Moving excel and csv file parsing code from client to core module

--- src/geneweaver/core/parse/exceptions.py
+++ src/geneweaver/core/parse/exceptions.py
@@ -7,6 +7,28 @@ class UnsupportedFileTypeError(Exception):
     pass
 
 
+class EmptyFileError(UnsupportedFileTypeError):
+    """Custom exception for when a file is empty.
+
+    Attributes
+    ----------
+        file_path -- the path of the file that is empty
+        message -- explanation of the error
+    """
+
+    def __init__(
+        self: "EmptyFileError", file_path: str, message: str = "File is empty."
+    ) -> None:
+        """Initialize the exception."""
+        self.file_path = file_path
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self: "EmptyFileError") -> str:
+        """Return a string representation of the exception."""
+        return f"{self.file_path} -> {self.message}"
+
+
 class NotAHeaderRowError(Exception):
     """Raised when a row is not a header row."""
 
---
+++ src/geneweaver/core/parse/utils.py
@@ -0,0 +1,65 @@
+"""Utility functions for the parser module."""
+from pathlib import Path
+from typing import Dict, List
+
+from geneweaver.core.parse.enum import FileType
+from geneweaver.core.types import StringOrPath
+
+
+def get_file_type(file_path: StringOrPath) -> FileType:
+    """Determine if a file at a given path is a csv or xlsx file.
+
+    :param file_path: Path to the file.
+
+    :returns: The file type.
+                - 'csv' if the file is a CSV file,
+                - 'xlsx' if the file is an Excel file.
+
+    :raises ValueError: If the file type is not supported.
+    """
+    file_path = Path(file_path)  # convert to Path object if not already
+    extension = file_path.suffix.lower()
+
+    if extension == ".csv":
+        return FileType.CSV
+    elif extension == ".xlsx":
+        return FileType.EXCEL
+    elif extension == ".txt":
+        return FileType.TEXT
+    else:
+        raise ValueError(f"Unsupported file type {extension}.")
+
+
+def read_file_content(file_path: StringOrPath) -> str:
+    """Read the content of the file at a given path.
+
+    :param file_path: Path to the file.
+
+    :returns: The content of the file as a string.
+
+    :raises FileNotFoundError: If the file does not exist.
+    :raises IsADirectoryError: If the path points to a directory.
+    :raises PermissionError: If the file is not readable.
+    """
+    with open(file_path, "r") as f:
+        content = f.read()
+
+    return content
+
+
+def replace_keys(
+    data: List[Dict[str, str]], new_keys: List[str]
+) -> List[Dict[str, str]]:
+    """Replace keys in a list of dictionaries.
+
+    :param data: Original list of dictionaries.
+    :param new_keys: List of new keys. The order should correspond to the order of
+    original keys.
+
+    :returns: A new list of dictionaries with replaced keys.
+    """
+    new_data = []
+    for row in data:
+        new_row = dict(zip(new_keys, row.values()))  # noqa: B905
+        new_data.append(new_row)
+    return new_data
