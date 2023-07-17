7fa4fc808e1bbf2b268afbbfa042075f38e4f155
Alexander Berger <alexander.berger@jax.org>
2023-07-16 21:20:27 -0400

    Adding ruff noqa tag for Any in add_to_dict_if_not_none func

--- src/geneweaver/core/utils.py
+++ src/geneweaver/core/utils.py
@@ -2,7 +2,7 @@
 from typing import Any
 
 
-def add_to_dict_if_not_none(d: dict, key: str, value: Any) -> None:
+def add_to_dict_if_not_none(d: dict, key: str, value: Any) -> None:  # noqa: ANN401
     """Add a value to a dictionary only if the value is not None.
 
     :param dict d: The dictionary to which the value should be added.
