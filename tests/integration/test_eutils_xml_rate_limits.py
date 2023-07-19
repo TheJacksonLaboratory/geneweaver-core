504dacdd77b50659d23bb21b8bab25668900adc6
Alexander Berger <alexander.berger@jax.org>
2023-07-19 15:30:41 -0400

    Updating eutils integration tests to use a different pubmedid on each request

--- tests/integration/test_eutils_xml_rate_limits.py
+++ tests/integration/test_eutils_xml_rate_limits.py
@@ -1,8 +1,9 @@
 """Test that the Eutils API rate limits don't break our usage."""
 import pytest
+import requests
 from geneweaver.core.publication.pubmed import PUBMED_SVC_URL, get_xml_for_pubmed_id
-from requests import get
 
+from tests.integration.const import PUBMED_IDS
 from tests.integration.utils import call_function
 
 N_RATE_TEST_ARGS = [
@@ -24,8 +25,8 @@ N_RATE_TEST_ARGS = [
 @pytest.mark.parametrize(("num_times", "rate"), N_RATE_TEST_ARGS)
 def test_rate_for_eutils_publication_endpoint(num_times, rate):
     """Test rate for eutils API endpoint used by get_xml_for_pubmed_id."""
-    url = PUBMED_SVC_URL.format("3981386")
-    results = call_function(get, num_times, rate, url)
+    urls = [PUBMED_SVC_URL.format(pubmed_id) for pubmed_id in PUBMED_IDS]
+    results = call_function(requests.get, num_times, rate, iterate_args=urls)
     for response in results:
         assert response.ok, response.text
 
@@ -33,6 +34,8 @@ def test_rate_for_eutils_publication_endpoint(num_times, rate):
 @pytest.mark.parametrize(("num_times", "rate"), N_RATE_TEST_ARGS)
 def test_rate_for_get_xml_for_pubmed_id(num_times, rate):
     """Test rate for get_xml_for_pubmed_id."""
-    results = call_function(get_xml_for_pubmed_id, num_times, rate, "3981386")
+    results = call_function(
+        get_xml_for_pubmed_id, num_times, rate, iterate_args=PUBMED_IDS
+    )
     for response in results:
         assert response is not None, response
